472bfc344586a3ab9c5ae4e0da26d0e0d88001e2
Kody Samaroo <kody.samaroo@gmail.com>
2020-11-22 05:25:51 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -5,13 +5,11 @@ def generate_star_date
   return (rand(100000) + 400000) / 10.0
 end
 
-def state_log(date)
-  "Captain's Log, star date #{date}."
+def state_log(star_date)
+  return "Captain's Log, star date #{star_date}."
 end
 
 def engage
   puts state_log(date)
   date = generate_star_date
-  binding.pry
-  puts 'a'
 end

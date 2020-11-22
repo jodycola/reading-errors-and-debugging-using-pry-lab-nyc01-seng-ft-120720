14e03dc69a45e421d0558116a628320451a167f2
Kody Samaroo <kody.samaroo@gmail.com>
2020-11-22 04:54:51 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -12,9 +12,9 @@ def prompt_user
 end
 
 def selection(num)
-  if num = 1
+  if num == 1
     puts "YUM YUM MUNCH MUNCH MUNCH"
-  else num = 2
+  else num == 2
     puts  "HAM HAM HAM IN MY TUMMY"
   end
   binding.pry

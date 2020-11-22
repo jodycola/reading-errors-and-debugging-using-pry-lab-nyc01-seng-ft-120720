8800cf29886f1afa1db52faf2c3ad669483f9598
Kody Samaroo <kody.samaroo@gmail.com>
2020-11-22 04:39:52 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,7 +1,8 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def get_user_input
-  gets.chomp
+  num = gets.chomp
 end
 
 def prompt_user
@@ -11,10 +12,10 @@ def prompt_user
 end
 
 def selection(num)
-  if num = 1
-     "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
-     "HAM HAM HAM IN MY TUMMY"
+  if num == 1
+    puts "YUM YUM MUNCH MUNCH MUNCH"
+  elsif num == 2
+    puts  "HAM HAM HAM IN MY TUMMY"
   end
 end
 
@@ -22,3 +23,4 @@ def runner
   prompt_user
   selection(get_user_input)
 end
+
--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,4 +1,5 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def start_game(player1, player2)
   "Hello #{player1} & #{playar2}"
@@ -7,3 +8,4 @@ end
 def play_game(player2)
   "#{player1} is better than #{player2}"
 end
+

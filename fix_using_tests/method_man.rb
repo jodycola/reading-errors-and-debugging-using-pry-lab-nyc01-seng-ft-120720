115427a6eabcaf475feec45af81bf6ebb37a32d9
Kody Samaroo <kody.samaroo@gmail.com>
2020-11-22 05:12:17 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -2,9 +2,7 @@
 require 'pry'
 
 def start_game(player1, player2)
-  return "Hello #{player1} & #{playar2}"
-  binding.pry
-  puts 'a'
+  return "Hello #{player1} & #{player2}"
 end
 
 def play_game(player1, player2)

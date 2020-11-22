adffa6fd488cb9e4d321d0e844dc6552d4eec9c0
Kody Samaroo <kody.samaroo@gmail.com>
2020-11-22 04:51:39 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -15,9 +15,8 @@ def selection(num)
   if num = 1
     puts "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
-    puts  "HAM HAM HAM IN MY TUMMY"
   binding.pry
-  puts 'h'
+    puts  "HAM HAM HAM IN MY TUMMY"
   end
 end
 
